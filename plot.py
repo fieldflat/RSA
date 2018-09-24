a2b9f3c68622ef2ecc356bb143d1f46617462b11
平田智紀 <tomonorihirata@hiratatomonori-no-MacBook-Air.local>
2018-09-24 15:28:17 +0900

    20180924 グラフの微修正

--- plot.py
+++ plot.py
@@ -5,60 +5,63 @@ import main
 d_length = main.d_length
 d_top_bit = main.d_top_bit
 d_weight = main.d_weight
+loop_times = main.Loop_times
 
 def plotting(mod_list, mult_list, title):
-    #print('mod_list: {0}\nmult_list: {1}'.format(mod_list, mult_list))
-    mod_list, mod_count_list = frequency(mod_list)
-    print(mod_count_list)
-    count = np.array(mod_count_list)
-    freq = np.array(mod_list)
-    plt.plot(freq, count, label='mod', marker='o')
 
-    mult_list, mult_count_list = frequency(mult_list)
-    print(mult_count_list)
-    count = np.array(mult_count_list)
-    freq = np.array(mult_list)
-    plt.plot(freq, count, label='mult', marker='x')
+    mod_count_list, mod_freq_list = frequency(mod_list)
+    mult_count_list, mult_freq_list = frequency(mult_list)
 
-    #plt.xlim(0, 10)
-    plt.title('d_length={0}, d_top_bit={1}, d_weight={2}, {3}'.format(d_length, d_top_bit, d_weight, title))
+    mod_list_length = len(mod_count_list)
+    mult_list_length = len(mult_count_list)
+
+    mod_count_list = np.pad(mod_count_list, [0, (mult_list_length - mod_list_length)], 'constant')
+    mod_freq_list =  np.pad(mod_freq_list, [0, (mult_list_length - mod_list_length)], 'constant')
+
+    mod_argmax = np.argmax(mod_freq_list)
+    mult_argmax = np.argmax(mult_freq_list)
+
+    print('最大値のインデックスは, mod: {0}, mult: {1}'.format(mod_argmax, mult_argmax))
+
+    mod_freq = np.array(mod_freq_list)
+    mod_count = np.array(mod_count_list)
+    plt.plot(mod_count, mod_freq, label='mod', marker='o')
+
+    mult_freq = np.array(mult_freq_list)
+    mult_count = np.array(mult_count_list)
+    plt.plot(mult_count, mult_freq, label='mult', marker='x')
+
+    """
+    sum_freq = mod_freq + mult_freq
+    sum_argmax = np.argmax(sum_freq)
+    print('最大値のインデックスは, sum: {0}'.format(sum_argmax))
+    plt.plot(mult_count, sum_freq, label='sum', marker='v')
+    """
+
+    plt.title('d_length={0}, d_top_bit={1}, d_weight={2}, {3}, LOOP={4}'.format(d_length, d_top_bit, d_weight, title, loop_times))
     plt.xlabel('count')
     plt.ylabel('frequency')
     plt.legend(loc = 'upper left')
     plt.show()
 
 def frequency(list):
+
+    # countの最大数を探索する.
     max = -1
     for item in list:
         if max <= item:
             max = item
 
-    new_list = [0] * (max+3)
+    # 0から(max+3)までの数字を順にcount_listに格納する(要はグラフのx軸となる).
+    freq_list = [0] * (max+3)
     count_list = []
     k = 0
-    for item in new_list:
+    for item in freq_list:
         count_list.append(k)
         k += 1
 
+    # freq_listに頻度を格納する.
     for item in list:
-        new_list[item] += 1
+        freq_list[item] += 1
 
-    zero_count = 0
-    for item in new_list:
-        if item == 0:
-            zero_count += 1
-        else:
-            break
-
-    if zero_count >= 2:
-        zero_count -= 2
-    else:
-        zero_count = 0
-
-
-    del count_list[:zero_count]
-    del new_list[:zero_count]
-
-
-
-    return count_list, new_list
+    return count_list, freq_list
